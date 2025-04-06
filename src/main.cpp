51cbe5afe1501daa6d75bbab96bb07d7b48e503f
Oliver Blaser <58989552+oblaser@users.noreply.github.com>
2025-04-06 21:37:44 +0200

    serialise ARP request package

--- src/main.cpp
+++ src/main.cpp
@@ -151,14 +151,14 @@ int main(int argc, char** argv)
         // args.push_back("192.168.1.200-254/26"); // help example
         // args.push_back("192.168.3.0-4.255"); // help example
 
-        // args.push_back("192.168.1.99");
+        args.push_back("192.168.1.120");
 
         // args.push_back("192.168.1.0/24");
         // args.push_back("192.168.1.0/26");
 
-        args.push_back("192.168.1.120/24");
+        // args.push_back("192.168.1.120/24");
 
-        args.push_back("192.168.1.120-140");
+        // args.push_back("192.168.1.120-140");
         // args.push_back("192.168.3.253-5.3");
         // args.push_back("10.55.3.253-5.3");
         // args.push_back("192.168.0.253-1.10/24");
